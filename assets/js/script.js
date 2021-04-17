2dc341568cd84e6f5f3a35f8aaf80f66e79a05f4
MichaelRosa9 <michaelrosa9@gmail.com>
2021-04-17 12:07:23 +0200

    function vname verification

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,34 @@
-//inserire nome oppure esce fuori un alert
+//inserire nome corretto oppure esce fuori un alert
 var burger_choice = ["hamburger", "cheeseburger", "bacon-cheese", "veggie burger"];
 
-var input_nameBurger = document.getElementById('name').value;
+//burger price
+var hamburger = 10;
+var cheeseburger = 12;
+var bacon_cheese = 13;
+var veggie_burger = 15;
+
+var input_nameBurger = document.getElementById('name');
 
 
-document.getElementById("btn").addEventListener(function() {
-  button_calculate;
+
+//var check_menu = verryfy_input_text(input_nameBurger, burger_choice);
+document.getElementById("button").addEventListener("click", function(){
+  verryfy_input_text(input_nameBurger, burger_choice);
+
 });
 
-var button_calculate = verryfy_input_text(input_nameBurger, burger_choice);
 
+
+//funzione verifica nome dentro un array
 function verryfy_input_text(str, arr){
 
-  if(!str.includes(arr)){
-    alert("We're sorry but that choice isn;t available in the Menu.\n Please chooese an item from the Menu.")
+  var input_value = str.value.trim();
+
+  if(!(arr.includes(input_value))){
+    alert("Non hai inserito corretamente quello ceh vuoi ordinare. scegli tra: " + arr)
+  }else{
+    alert("coretct!")
   }
-    
-  console.log(input_nameBurger);
 }
-//se 
+
+//funzione prezzo
