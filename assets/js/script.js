a91b02639db24d9008bfad98c90daa4c50d0c2a1
MichaelRosa9 <michaelrosa9@gmail.com>
2021-04-18 12:19:30 +0200

    ingredienrts work in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,72 @@
 //inserire nome corretto oppure esce fuori un alert
 var burger_choice = ["hamburger", "cheeseburger", "bacon-cheese", "veggie burger"];
 
-//burger price
-var hamburger = 10;
-var cheeseburger = 12;
-var bacon_cheese = 13;
-var veggie_burger = 15;
-
 var input_nameBurger = document.getElementById('name');
 
+var ingredients = document.getElementsByClassName("ingredient-checkbox");
+var price_info = document.getElementById("price");
+
+var default_price = 10;
 
 
-//var check_menu = verryfy_input_text(input_nameBurger, burger_choice);
+console.log(ingredients);
+
+
+//esecuzione calcolo con il click
 document.getElementById("button").addEventListener("click", function(){
   verryfy_input_text(input_nameBurger, burger_choice);
+  var calc_ingridients = check_ingredients(ingredients);
+
+  var total_price = calc_bill(default_price, calc_ingridients);
+
+  bill_display(total_price, price_info);
 
 });
 
 
 
-//funzione verifica nome dentro un array
+
+//funzione che mostra il prezzo sull'html
+function bill_display(total, target_price){
+  target_price.innerHTML = total;
+}
+
+//funzione prezzo totale
+function calc_bill(num1, num2){
+  var tot = num1 + num2;
+  return tot;
+}
+
+//funzione per il risultato e la somma totale di solo gli ingredienti
+function check_ingredients(arr){
+
+  var sum = 0;
+
+  for(var i = 0; i< arr.length; i++){
+
+    var ingredient = arr[i];
+
+    if(ingredient.checked === true){
+
+      sum += parseInt(ingredient.value);
+
+    }else{
+
+      sum -= parseInt(ingredient.value);
+
+    }
+
+  }
+  return sum;
+}
+
+
+//funzione verifica se il nome inserito é presente nell'array
 function verryfy_input_text(str, arr){
 
   var input_value = str.value.trim();
 
   if(!(arr.includes(input_value))){
     alert("Non hai inserito corretamente quello ceh vuoi ordinare. scegli tra: " + arr)
-  }else{
-    alert("coretct!")
   }
 }
-
-//funzione prezzo
